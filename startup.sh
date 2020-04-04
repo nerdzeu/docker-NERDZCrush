c4267552966a6728f12a4144fe1b452e21dc087f
Paolo Galeone <nessuno@nerdz.eu>
2020-04-04 18:46:32 +0200

    change startupp script

--- startup.sh
+++ startup.sh
@@ -5,18 +5,17 @@ chown -R mediacrush:mediacrush /home/mediacrush
 if [ ! -d /home/mediacrush/MediaCrush ]; then
     cd /home/mediacrush
     git clone https://github.com/nerdzeu/NERDZCrush MediaCrush
-    cd MediaCrush
-
-    virtualenv . --python=python2
-    source bin/activate
-    pip2 install -r requirements.txt
 fi
+
+cd /home/mediacrush/MediaCrush
+
+virtualenv . --python=python2
+source bin/activate
+pip2 install -r requirements.txt
 
 if [ ! -d /home/mediacrush/storage ]; then
     mkdir /home/mediacrush/storage
 fi
-
-cd /home/mediacrush/MediaCrush
 
 source bin/activate
 python compile_static.py
